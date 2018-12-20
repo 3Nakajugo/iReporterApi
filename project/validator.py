7a7eb75b6b320ea6fbc8edee54fdcdd3813a324a
edna <edna@rr.com>
2018-12-20 12:17:51 +0300

    add validations to incident [Delivers #162751803]

--- project/validator.py
+++ project/validator.py
@@ -1,4 +1,23 @@
-class Validator:
 
-    def validate_incident(self):
+
+class Validator:
+    """
+    method to validate incident
+    """
+
+    def validate_incident(self, created_by, incident_type, location, status, file, comment):
+        if not created_by and created_by == "":
+            return "Name of creator is missing"
+        if not incident_type and incident_type == "":
+            return "please mark the incident as redflag or intervention"
+        if not location and location == "":
+            return "location is missing"
+        if not status and status == "":
+            return "status is missing"
+        if not file and file == "":
+            return "An image or video is missing"
+        if not comment and comment == "":
+            return"comment is missing"
+
+    def validate_user(self):
         pass
--- project/views.py
+++ project/views.py
@@ -3,11 +3,16 @@ import datetime
 from flask import Flask, jsonify, request
 from .models import Incident, User, incidents
 from .controllers.incident_cntr import IncidentCntr
+from .validator import Validator
 
 incident_controller = IncidentCntr()
+incident_validator = Validator()
 
 app = Flask(__name__)
 
+"""
+route for home page
+"""
 
 @app.route('/api/v1/welcome')
 def index():
@@ -15,6 +20,10 @@ def index():
                     "status": 200})
 
 
+
+"""
+route for posting/creating an incident
+"""
 @app.route('/api/v1/incidents', methods=['POST'])
 def create_incident():
     request_data = request.get_json()
@@ -26,6 +35,10 @@ def create_incident():
     status = request_data.get('status')
     file = request_data.get('file')
     comment = request_data.get('comment')
+    valid_incident = incident_validator.validate_incident(created_by,
+                                                          incident_type, location, status, file, comment)
+    if valid_incident:
+        return jsonify({"status": 400, "message": "some fields are empty"}), 400
     incident_obj = Incident(incident_id, date, created_by,
                             incident_type, location, status, file, comment)
 
@@ -39,4 +52,4 @@ def create_incident():
         file=incident_obj.file,
         comment=incident_obj.comment)
     if add_incident:
-        return jsonify({"status": 201, "data": incidents, "message": "incident {} has been created".format(incident_type)}),201
+        return jsonify({"status": 201, "data": incidents, "message": "incident {} has been created".format(incident_type)}), 201
