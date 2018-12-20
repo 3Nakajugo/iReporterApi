f848e60dbdae1c618a9f72f2d322ec4ee97576ca
edna <edna@rr.com>
2018-12-20 14:13:17 +0300

    update views.py ,add get all endpoint [Delivers #162782055]

--- project/controllers/incident_cntr.py
+++ project/controllers/incident_cntr.py
@@ -3,14 +3,11 @@ from project.models import Incident, incidents
 
 class IncidentCntr:
 
-    def create_incident(self, incident_id, date, created_by, incident_type, location, status, file, comment):
+    def create_incident(self, created_by, incident_type, location, file, comment):
         incident = Incident(
-            incident_id=incident_id,
-            date=date,
             created_by=created_by,
             incident_type=incident_type,
             location=location,
-            status=status,
             file=file,
             comment=comment
         )
@@ -18,4 +15,8 @@ class IncidentCntr:
         incidents.append(new_incident)
         return incidents
 
-
+    def get_all_redflags(self):
+        if len(incidents) > 0:
+            for incident in incidents:
+                return incident
+        
