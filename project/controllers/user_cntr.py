27de2b8f0b232d7a1db965c3747b4430a8e98145
edna <edna@rr.com>
2019-01-16 22:53:35 +0300

    update user model

--- project/controllers/user_cntr.py
+++ project/controllers/user_cntr.py
@@ -6,7 +6,7 @@ class UserController:
     def create_user(self, user):
         new_user = user.user_to_json()
         users.append(new_user)
-        return user
+        return new_user
 
     def get_all_users(self):
         # for user in users:
